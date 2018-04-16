9951b5031e6bdc0728a39020d1188bcad01053ba
richardcalata <calatar6@gmail.com>
2018-04-16 09:29:25 -0500

    I thinks it's functionally complete

---
+++ js/exercise.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is you age?");
+var ageInDays = age * 365;
+console.log("Your full name is " + firstName + " " + lastName + ".");
+console.log("You are " + ageInDays + " days old." );
--- js/simonJS.js
+++ js/simonJS.js
@@ -5,20 +5,56 @@ var userArr = [];
 var colorNumber;
 var color;
 var level =0;
+var roundCount = 0;
 
 ///causes color panels to react to user mouse clicks///
 
 $('.quarter').on('mousedown', function(){
-    $(this).addClass($(this).attr('class').split(' ')[3]+'-active');
 
-        console.log($(this).attr('class').split(' ')[3] + " was clicked");
-        // userArr.push(useClick);
-        console.log($(this).attr('class').split(' ')[3]+ '-active');
+    colorNumber = $(this).attr('id');
+    color =$(this).attr('class').split(' ')[3];
+    userArr.push(colorNumber);
+    lightPanel(colorNumber,color);
 
-    $(this).on('mouseup', function(){
-            // console.log($(this).attr('class').split(' ')[1] + " was UNclicked");
-            $(this).removeClass($(this).attr('class').split(' ')[3]+ '-active');
-        });
+    if(!checkPlayerInput()){
+        displayError();
+        userArr = [];
+        simonArr = [];
+        level= 0;
+
+    }
+    if(simonArr.length > 0 && userArr.length == simonArr.length) {
+        level++;
+        roundCounter();
+        console.log("this is the length of roundCount array "+roundCount);
+        userArr =[];
+        // console.log("a very important message")
+        setTimeout(function(){
+            startGame();
+            }, 800);
+
+    }
+
+
+    function roundCounter(){
+        if(userArr.length == simonArr.length){
+        roundCount++;
+            console.log("round Counter fires");
+            $(".round_count").text("Highest Round " + roundCount);
+
+
+        }
+    }
+    // $(this).addClass($(this).attr('class').split(' ')[3]+'-active');
+    //
+    //     console.log($(this).attr('class').split(' ')[3] + " was clicked");
+    //     // userArr.push(useClick);
+    //     console.log($(this).attr('class').split(' ')[3]+ '-active');
+    //
+    // $(this).on('mouseup', function(){
+    //         // console.log($(this).attr('class').split(' ')[1] + " was UNclicked");
+    //         $(this).removeClass($(this).attr('class').split(' ')[3]+ '-active');
+    //     });
 });
 
 $('.start').on('click', function(){
@@ -28,22 +64,25 @@ $('.start').on('click', function(){
 
 // starts game
 function startGame(){
-    console.log(level);
-    $(".game_body").text(level);
+
+    // console.log("current level "+level);
+    $(".game_body").text("Round " +level);
     randomNumber();
     var i=0;
     var interval = setInterval(function(){
+    // console.log(roundCount[i]);
      colorNumber =  simonArr[i];
      color = $("#" + colorNumber).attr('class').split(' ')[3];
-     console.log($("#"+colorNumber).attr('class')[3]);
-     console.log(color);
+     // console.log($("#"+colorNumber).attr('class')[3]);
+     // console.log(color);
      i++;
      lightPanel(colorNumber,color);
+
     if(i==simonArr.length){
         clearInterval(interval);
     }
 
-    }, 1000);
+    }, 800);
 
 }
 
@@ -55,10 +94,36 @@ function randomNumber(){
 
 ///add class
 function lightPanel(colorNumber,color){
-    console.log(" an Important message")
+    // console.log("lightPanel function")
 
-    $("#"+colorNumber).addClass(color+"-active");
+    $("#"+colorNumber).addClass(color+"-lit");
     setTimeout(function(){
-        $("#"+colorNumber).removeClass(color+"-active");
+        $("#"+colorNumber).removeClass(color+"-lit");
     }, 400);
+}
+
+function checkPlayerInput(){
+    for(var i= 0; i<userArr.length; i++) {
+        if(userArr[i] != simonArr[i]){
+            return false;
+
+        }
+    } return true;
+}
+
+function displayError(){
+    $('.game_body').text("Error Game will restart");
+    var counter = 0;
+
+var errorTimer = setInterval(function() {
+    $('.btn-lg').addClass('error');
+    $('.quarter').addClass('error');
+    counter++;
+    if(counter == 3){
+        clearInterval(errorTimer);}
+    setTimeout(function () {
+        $('.btn-lg').removeClass('error');
+        $('.quarter').removeClass('error')
+    }, 200)
+}, 400)
 }
