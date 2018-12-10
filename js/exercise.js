57f356db573cc26d7254ff58b71e3f8015a86433
richardcalata <calatar6@gmail.com>
2018-12-10 12:29:32 -0600

    spelling

--- js/exercise.js
+++ js/exercise.js
@@ -1,6 +1,6 @@
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
-var age = prompt("What is you age?");
+var age = prompt("What is your age?");
 var ageInDays = age * 365;
 console.log("Your full name is " + firstName + " " + lastName + ".");
 console.log("You are " + ageInDays + " days old." );
